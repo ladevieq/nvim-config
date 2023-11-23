cabc27563ab8a203036c550cd713c518b639c74b
Quentin Ladevie <qladevie@virtuos.com.cn>
2023-11-23 15:01:24 +0100

    Fix c/cpp indentation

--- init.lua
+++ init.lua
@@ -211,8 +211,37 @@ require('lualine').setup {
 }
 
 
-require('nvim-autopairs').setup({})
+require('nvim-autopairs').setup({
+    "windwp/nvim-autopairs",
+    dependencies = {
+        { "hrsh7th/nvim-cmp" },
+    },
+    event = "InsertEnter",
+    opts = {},
+    config = function(_, opts)
+        local npairs = require("nvim-autopairs")
+        local rule = require("nvim-autopairs.rule")
+        local cond = require("nvim-autopairs.conds")
 
+        npairs.setup(opts)
+
+        local is_template = require("util.pair").is_template
+        local semicolon = require("util.pair").struct_class_semicolon
+
+        npairs.add_rules({
+            rule("<", ">"):with_pair(cond.none()):with_move(cond.done()):use_key(">"),
+            rule("{", "};", { "cpp", "c" }):with_pair(semicolon),
+        })
+
+        vim.keymap.set("i", "<", is_template)
+    end,
+})
+
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
 
 -- ----------------------------------
 --           FZF
