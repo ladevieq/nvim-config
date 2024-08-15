7d8378afb36981fe7f3997112de9b94f32ce45bb
ladevieq <ladevie.quentin@gmail.com>
2024-08-16 01:02:58 +0200

    Automatically open quickfix window upon compilation

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -54,4 +54,4 @@ function _G.bufferLspMappings(client, bufnr)
     api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rr',   '<cmd>lua vim.lsp.stop_client(vim.lsp.get_active_clients())<CR>', opts)
 end
 
-api.nvim_set_keymap('n', '<F5>', '<cmd>silent AsyncRun -program=make<cr>', {})
+api.nvim_set_keymap('n', '<F5>', '<cmd>silent vert copen 75 | AsyncRun -program=make<cr>', {})
